0483bd524290771cb3706e2a2b3a083442a0bc40
Mikko Metso <mikkosanterimetso@gmail.com>
2018-01-28 11:43:53 +0200

    added a findAllByStationIs -method for shortlisting the perceptions by stationName

--- src/main/java/reaktor/weatherapp/dao/PerceptionDAO.java
+++ src/main/java/reaktor/weatherapp/dao/PerceptionDAO.java
@@ -2,12 +2,16 @@ package reaktor.weatherapp.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import reaktor.weatherapp.model.Perception;
+import reaktor.weatherapp.model.Station;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
-public interface PerceptionDAO extends CrudRepository<Perception, Long>{
+public interface PerceptionDAO extends CrudRepository<Perception,Long>{
 
-    public Perception findByPerceptionId(Long perceptionId);
+    public List<Perception> findAll();
+
+    public List<Perception> findAllByStationIs(Station station);
 
 }
