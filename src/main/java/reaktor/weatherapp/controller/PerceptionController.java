3c4c8290f9a70d9cb0e78ae7a96b25ce62ad1c4a
Mikko Metso <mikkosanterimetso@gmail.com>
2018-01-31 23:30:17 +0200

    Added query methods to get MAX and MIN temperatures from database table by stationName. Also started working with quering the latest submission to table by selected stationName

--- src/main/java/reaktor/weatherapp/controller/PerceptionController.java
+++ src/main/java/reaktor/weatherapp/controller/PerceptionController.java
@@ -5,14 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.util.DateUtils;
 import reaktor.weatherapp.dao.PerceptionDAO;
 import reaktor.weatherapp.model.Perception;
 import reaktor.weatherapp.model.Station;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 @CrossOrigin(maxAge = 3600)
 @Controller
@@ -21,6 +18,7 @@ public class PerceptionController {
     @Autowired
     private PerceptionDAO perceptionDAO;
 
+    // Creates a new Perception with current timestamp, temperature and pre-defined enum value of Station.
     @RequestMapping(value = "/perceptions", method = RequestMethod.POST)
     @ResponseBody
     public String create(double temperature, Station station){
@@ -35,7 +33,8 @@ public class PerceptionController {
         return "Hooray! Your perception was successfully posted";
     }
 
-    @RequestMapping(value = "/perceptions", method = RequestMethod.GET)
+    // Lists all the perceptions in database as JSON
+    @RequestMapping(value = "/all/perceptions", method = RequestMethod.GET)
     @ResponseBody
     public String displayAllPerceptions() {
         String response = "";
@@ -44,7 +43,8 @@ public class PerceptionController {
         return response;
     }
 
-    @RequestMapping(value = "/{stationName}", method = RequestMethod.GET)
+    // Lists all the perceptions by given station name in the URL as JSON
+    @RequestMapping(value = "/all/{stationName}", method = RequestMethod.GET)
     @ResponseBody
     public String displayPerceptionsFromStation(@PathVariable String stationName) {
         String response = "";
@@ -53,28 +53,48 @@ public class PerceptionController {
         return response;
     }
 
-    @RequestMapping(value = "/test/{stationName}", method = RequestMethod.GET)
-    @ResponseBody
-    public String displayLatestTemp(@PathVariable String stationName) {
-        String response = "";
-        Iterable<Perception> perceptionByStation = perceptionDAO.findLatestTempByStation(Station.valueOf(stationName));
-        response = response + perceptionByStation.toString();
-        return response;
-    }
-
-    @RequestMapping(value = "/min/{stationName}", method = RequestMethod.GET)
+    // Lists the latest
+    @RequestMapping(value = "/latest/{stationName}", method = RequestMethod.GET)
     @ResponseBody
     public String displayMinAndMaxByStation(@PathVariable String stationName) {
         Date yesterday = new Date(System.currentTimeMillis() - 1000L * 60L * 60L * 24L);
+
+        // This section is for printing out the latest submitted perception for the given station
+        String perceptionId;
+        String perceptionTemp;
+        String perceptionTimestamp;
         String response = "";
+
+        Iterable<Perception> perceptionIdByStation = perceptionDAO.findLatestPerceptionIdByStation(Station.valueOf(stationName));
+        Iterable<Perception> perceptionTempByStation = perceptionDAO.findLatestTempByStation(Station.valueOf(stationName));
+        Iterable<Perception> perceptionTimestampByStation = perceptionDAO.findLatestTimestampByStation(Station.valueOf(stationName));
+
+        //Formatting out some unnecessary brackets from sql query
+        perceptionId = perceptionIdByStation.toString();
+        perceptionId = perceptionId.substring(1, perceptionId.length() -1);
+        perceptionTemp = perceptionTempByStation.toString();
+        perceptionTemp = perceptionTemp.substring(1, perceptionTemp.length() -1);
+        perceptionTimestamp = perceptionTimestampByStation.toString();
+        perceptionTimestamp = perceptionTimestamp.substring(1, perceptionTimestamp.length() -1);
+
+        response = response + "[{\"perceptionId\" : \"" + perceptionId + "\",\"temperature\" : \"" + perceptionTemp + "\",\"timestamp\" : \"" + perceptionTimestamp + "\"},";
+
+        // And this section is for printing out the max/min temperatures for the given station for the last 24 hours
+        String perceptionMin;
+        String perceptionMax;
+
         Iterable<Perception> stationMin = perceptionDAO.findMinTempByStation(Station.valueOf(stationName), yesterday);
         Iterable<Perception> stationMax = perceptionDAO.findMaxTempByStation(Station.valueOf(stationName), yesterday);
-        response = response + "min : " + stationMin.toString() + " / max : " + stationMax.toString();
+
+        //Formatting out some unnecessary brackets from sql query
+        perceptionMin = stationMin.toString();
+        perceptionMin = perceptionMin.substring(1, perceptionMin.length() -1);
+        perceptionMax = stationMax.toString();
+        perceptionMax = perceptionMax.substring(1, perceptionMax.length() -1);
+
+        response = response + "{\"min\" : \"" + perceptionMin + "\", \"max\" : \"" + perceptionMax + "\"}]";
         return response;
     }
-
-
-
 
 }
 
--- src/main/java/reaktor/weatherapp/dao/PerceptionDAO.java
+++ src/main/java/reaktor/weatherapp/dao/PerceptionDAO.java
@@ -1,10 +1,13 @@
 package reaktor.weatherapp.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import reaktor.weatherapp.model.Perception;
 import reaktor.weatherapp.model.Station;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 
 @Transactional
@@ -13,5 +16,23 @@ public interface PerceptionDAO extends CrudRepository<Perception,Long>{
     public List<Perception> findAll();
 
     public List<Perception> findAllByStationIs(Station station);
+
+    // Queries for printing out the properties of the last submitted perception by given station name
+    @Query(value = "SELECT temperature FROM Perception p WHERE p.perceptionId = (SELECT MAX(perceptionId) FROM Perception p WHERE p.station = :station)")
+    public List<Perception> findLatestTempByStation(@Param("station") Station station);
+
+    @Query(value = "SELECT perceptionId FROM Perception p WHERE p.perceptionId = (SELECT MAX(perceptionId) FROM Perception p WHERE p.station = :station)")
+    public List<Perception> findLatestPerceptionIdByStation(@Param("station") Station station);
+
+    @Query(value = "SELECT timestamp FROM Perception p WHERE p.perceptionId = (SELECT MAX(perceptionId) FROM Perception p WHERE p.station = :station)")
+    public List<Perception> findLatestTimestampByStation(@Param("station") Station station);
+
+
+    // Queries for printing out the min/max temperatures for the last 24 hours by given station name
+    @Query(value = "SELECT MIN(temperature) FROM Perception p WHERE p.station = :station AND p.timestamp >= :yesterday")
+    public List<Perception> findMinTempByStation(@Param("station") Station station, @Param("yesterday") Date yesterday);
+
+    @Query(value = "SELECT MAX(temperature) FROM Perception p WHERE p.station = :station AND p.timestamp >= :yesterday")
+    public List<Perception> findMaxTempByStation(@Param("station") Station station, @Param("yesterday") Date yesterday);
 
 }
