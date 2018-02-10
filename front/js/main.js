db50c08498f0881428d5a959a68f8232ecaa72d1
Mikko Metso <mikkosanterimetso@gmail.com>
2018-02-10 16:20:39 +0200

    Created a getApiData function to populate correct span tags with db-data. Also started working with form POST.

--- front/js/main.js
+++ front/js/main.js
@@ -1,56 +1,54 @@
-
-const amsterdamLatest = document.querySelector("#amsterdam-latest");
-const dubaiLatest = document.querySelector("#dubai-latest");
-const helsinkiLatest = document.querySelector("#helsinki-latest");
-const new_yorkLatest = document.querySelector("#new_york-latest");
-const tokyoLatest = document.querySelector("#tokyo-latest");
-
 const form = document.querySelector("#postPerceptions");
+const station = document.querySelector("#station");
+const temperature = document.querySelector("#temperature");
 
-const url = "http://207.154.212.16:8080/weather-app/all/perceptions";
+var apiUrls = { 
+	amsterdam : "http://207.154.212.16:8080/weather-app/latest/amsterdam",
+ 	dubai : "http://207.154.212.16:8080/weather-app/latest/dubai",
+ 	helsinki : "http://207.154.212.16:8080/weather-app/latest/helsinki",
+ 	new_york : "http://207.154.212.16:8080/weather-app/latest/new_york",
+ 	tokyo : "http://207.154.212.16:8080/weather-app/latest/tokyo"
+}
 
-fetch(url)
+const postPerception = "http://207.154.212.16:8080/weather-app/perceptions";
+
+var getApiData = function () {
+	for(key in apiUrls){
+	let temp = key; // Name of the current property e.g amsterdam
+    fetch(apiUrls[key]) // Value of the current property
 	.then((res) => { return res.json() })
             .then((data) => {
-            	console.log(data[0].temperature);
-            	for(var i = 0, length1 = data.length; i < length1; i++) {
-	            	if (data[i].station == "dubai") {
-	            		console.log(data[i].temperature);
-						amsterdamLatest.innerHTML = data[i].temperature
-					}
-				}
+            	document.querySelector(`#${temp}-latest`).innerHTML = data[0].temperature;
+            	document.querySelector(`#${temp}-date`).innerHTML = data[0].timestamp;
 			});
+	}
+	console.log('Api data updated')
+};
+
+getApiData();
 
 form.addEventListener("submit", function (e) {
 	e.preventDefault();
-	console.log(e.content);
-	console.log('testi')
-	// fetch(url, {
-	//   method: 'POST', // or 'PUT'
-	//   body: JSON.stringify(), 
-	//   headers: new Headers({
-	//     'Content-Type': 'application/json'
-	//   })
-	// }).then(res => res.json())
-	// .catch(error => console.error('Error:', error))
-	// .then(response => console.log('Success:', response));
+
+	let fd = new FormData();
+	fd.set("station", station.value);
+	fd.set("temperature", temperature.value);
+
+	fetch(postPerception, {
+	method: 'POST', // or 'PUT'
+	mode: 'no-cors',
+	body: fd, 
+	headers: new Headers({
+		'Content-Type': 'application/json'
+	})
+	}).then(res => console.log('Data succefully sent'))
+	.catch(error => console.error('Error:', error))
 })
 
-// $.getJSON("http://207.154.212.16:8080/weather-app/all/perceptions", function( data ) {	
-// 	for(var i = 0, length1 = data.length; i < length1; i++) {
-// 		if (data[i].station == "amsterdam") {
-// 			amsterdamLatest.innerHTML = data[4].temperature
-// 		} else if (data[i].station == "dubai") {
-// 			dubaiLatest.innerHTML = data[3].temperature
-// 		} else if (data[i].station == "helsinki") {
-// 			helsinkiLatest.innerHTML = data[2].temperature
-// 		} else if (data[i].station == "new_york") {
-// 			new_yorkLatest.innerHTML = data[5].temperature
-// 		} else if (data[i].station == "tokyo") {
-// 			tokyoLatest.innerHTML = data[7].temperature
-// 		}
-// 	}			
-// });
+// TODO: call getApiData() again after submit to update the new data to DOM
+
+
+
 
 
 
