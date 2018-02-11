165987dbac1a80a844ef77e876d5e11a1b78fbbb
Mikko Metso <mikkosanterimetso@gmail.com>
2018-02-11 18:27:44 +0200

    Got data to update after submit. Also added eventListeners to show / hide popup that prints a list of all the data

--- front/js/main.js
+++ front/js/main.js
@@ -1,7 +1,8 @@
-const form = document.querySelector("#postPerceptions");
+const form = document.querySelector("#postObservations");
 const station = document.querySelector("#station");
 const temperature = document.querySelector("#temperature");
 
+// A object for collecting all the stations API urls; 
 var apiUrls = { 
 	amsterdam : "http://207.154.212.16:8080/weather-app/latest/amsterdam",
  	dubai : "http://207.154.212.16:8080/weather-app/latest/dubai",
@@ -10,23 +11,56 @@ var apiUrls = {
  	tokyo : "http://207.154.212.16:8080/weather-app/latest/tokyo"
 }
 
-const postPerception = "http://207.154.212.16:8080/weather-app/perceptions";
+const postObservation = "http://207.154.212.16:8080/weather-app/perceptions";
+const allObservations = "http://207.154.212.16:8080/weather-app/all/perceptions"
 
+
+/* Function for fetching the API-data and appending it to DOM via correct span tags*/
 var getApiData = function () {
 	for(key in apiUrls){
 	let temp = key; // Name of the current property e.g amsterdam
     fetch(apiUrls[key]) // Value of the current property
 	.then((res) => { return res.json() })
             .then((data) => {
+            	// Appends fetched data to correct span by property 
             	document.querySelector(`#${temp}-latest`).innerHTML = data[0].temperature;
             	document.querySelector(`#${temp}-date`).innerHTML = data[0].timestamp;
+            	// Checks if api data exist (hence the apostrophes) and after that appends it to correct span
+            	if (data[1].max !== "null") {
+            	document.querySelector(`#${temp}-max-min`).innerHTML = "Max: " + data[1].max + " Min: " + data[1].min;
+            	}          	
 			});
 	}
 	console.log('Api data updated')
 };
 
-getApiData();
 
+/* Event Listener for catching clicks on "All observations" -link and printing
+them out to a pop up window with the help of toggling css class showPopups */
+document.querySelector("#listObservations").addEventListener("click", function (e) {
+	document.querySelector(".popups").classList.toggle("showPopups");
+
+	fetch(allObservations)
+	.then((res) => { return res.json() })
+            .then((data) => {
+            	let printString = "<p class=\"close\">&#10006;</p><h3>All the Observations</h3><p class=\"blackFont\">";
+            	for (var i = 0; i < data.length; i++) {
+            	printString = printString + "Station: " + data[i].station + 
+				" | Temperature: " + data[i].temperature + 
+				" | Time: " + data[i].timestamp + "<br>";				
+            	}
+            	printString = printString + "</p>"
+            	document.querySelector("#ObservationsBox").innerHTML = printString;
+            });
+})
+
+/* For hiding the previous popup -window with a click */
+document.querySelector(".popups").addEventListener("click", function (e) {
+	document.querySelector(".popups").classList.toggle("showPopups");
+	document.querySelector("#ObservationsBox").innerHTML = "";
+})
+
+/* Event Listener for submitting data to API with http POST */
 form.addEventListener("submit", function (e) {
 	e.preventDefault();
 
@@ -34,7 +68,7 @@ form.addEventListener("submit", function (e) {
 	fd.set("station", station.value);
 	fd.set("temperature", temperature.value);
 
-	fetch(postPerception, {
+	fetch(postObservation, {
 	method: 'POST', // or 'PUT'
 	mode: 'no-cors',
 	body: fd, 
@@ -43,9 +77,11 @@ form.addEventListener("submit", function (e) {
 	})
 	}).then(res => console.log('Data succefully sent'))
 	.catch(error => console.error('Error:', error))
+
+	document.location.reload(true);
 })
 
-// TODO: call getApiData() again after submit to update the new data to DOM
+getApiData();
 
 
 
