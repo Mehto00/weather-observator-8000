cd6f4529f27b43b72a6861645ea91f47faed88e9
Mikko Metso <mikkosanterimetso@gmail.com>
2018-02-20 16:37:05 +0200

    Started refactoring Controller for a better Api end points

--- src/main/java/reaktor/weatherapp/controller/ObservationController.java
+++ src/main/java/reaktor/weatherapp/controller/ObservationController.java
@@ -1,27 +1,25 @@
 package reaktor.weatherapp.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reaktor.weatherapp.dao.ObservationDAO;
 import reaktor.weatherapp.model.Observation;
 import reaktor.weatherapp.model.Station;
 
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 @CrossOrigin(origins = "http:// - ENABLE-CORS-FROM-URL - ")
-@Controller
+@RestController
 public class ObservationController {
 
     @Autowired
     private ObservationDAO observationDAO;
 
     // Creates a new Observation with current timestamp, temperature and pre-defined enum value of Station.
-    @RequestMapping(value = "/post-observation", method = RequestMethod.POST)
+    @RequestMapping(value = "/observations", method = RequestMethod.POST)
     @ResponseBody
     public String create(double temperature, Station station){
         Date timeStamp = new Date();
@@ -36,72 +34,125 @@ public class ObservationController {
         return "Hooray! Your observation was successfully posted";
     }
 
-    // Lists all the observations in database as JSON
-    @RequestMapping(value = "/all/observations", method = RequestMethod.GET)
+    // Lists all the observations from database as JSON
+    @RequestMapping(value = "/observations", method = RequestMethod.GET)
     @ResponseBody
     public String displayAllObservation() {
-        String response = "";
         Iterable<Observation> observation = observationDAO.findAll();
-        response = response + observation.toString();
-        return response;
+        observation.forEach(observation1 -> observation1.setFormatedDate(observation1.getTimestamp()));
+        String displayObservation = observation.toString();
+        return displayObservation;
     }
 
-    // Lists all the observations by given station name in the URL as JSON
-    @RequestMapping(value = "/all/{stationName}", method = RequestMethod.GET)
+    // Lists all the observations of one station by given URL as JSON
+    @RequestMapping(value = "/observations/{stationName}", method = RequestMethod.GET)
     @ResponseBody
     public String displayObservationsFromStation(@PathVariable String stationName) {
-        String response = "";
         Iterable<Observation> observationByStation = observationDAO.findAllByStationIs(Station.valueOf(stationName));
-        response = response + observationByStation.toString();
-        return response;
+        observationByStation.forEach(observation1 -> observation1.setFormatedDate(observation1.getTimestamp()));
+        String displayObservation = observationByStation.toString();
+        return displayObservation;
     }
 
-    // Lists the latest
+    // Lists the all latest observations by station
+    @RequestMapping(value = "/latest", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<ArrayList> displayAllMinAndMaxFromStations() {
+
+        ArrayList<ArrayList> displayAllMinAndMaxFromStations = new ArrayList<>();
+
+        // Loops through Station enum and adds data to displayAllMinAndMaxFromStations List
+        for (Station station : Station.values()) {
+            ArrayList<String> displayMinAndMaxByStation = new ArrayList<>();
+
+            Date yesterday = new Date(System.currentTimeMillis() - 1000L * 60L * 60L * 24L);
+
+            //Pattern and SimpleDateFormat for formating
+            String pattern = "dd/MM/yyyy HH:mm";
+            SimpleDateFormat df = new SimpleDateFormat(pattern);
+
+            // Fetch all necessary data from db with value Station.valueOf(stationName)
+            Iterable<Observation> observationIdByStation = observationDAO.findLatestObservationIdByStation(station);
+            Iterable<Observation> observationTempByStation = observationDAO.findLatestTempByStation(station);
+            Date observationTimestampByStation = observationDAO.findLatestTimestampByStation(station);
+            Iterable<Observation> stationMin = observationDAO.findMinTempByStation(station, yesterday);
+            Iterable<Observation> stationMax = observationDAO.findMaxTempByStation(station, yesterday);
+
+            // Format fetched data into a String and cut out unnecessary brackets from the beginning and end
+            // FetchData.toString().substring(1, FetchData.toString().length() -1);
+            String stationNameToCollection = station.toString();
+            String observationId = observationIdByStation.toString().substring(1, observationIdByStation.toString().length() -1);
+            String observationTemp = observationTempByStation.toString().substring(1, observationTempByStation.toString().length() -1);
+
+            String observationTimestamp;
+            try {
+                observationTimestamp = df.format(observationTimestampByStation).substring(1, df.format(observationTimestampByStation).length() -1);
+            } catch (Exception e) {observationTimestamp = "";}
+
+            String observationMin = stationMin.toString().substring(1, stationMin.toString().length() -1);
+            String observationMax = stationMax.toString().substring(1, stationMax.toString().length() -1);
+
+            // Create a list of Strings to print out and add the previous values to it
+            ArrayList<String> latestByStation = new ArrayList<String>();
+
+            displayMinAndMaxByStation.add(stationNameToCollection);
+            displayMinAndMaxByStation.add(observationId);
+            displayMinAndMaxByStation.add(observationTemp);
+            displayMinAndMaxByStation.add(observationTimestamp);
+            displayMinAndMaxByStation.add(observationMin);
+            displayMinAndMaxByStation.add(observationMax);
+
+            displayAllMinAndMaxFromStations.add(displayMinAndMaxByStation);
+        }
+
+        return displayAllMinAndMaxFromStations;
+    }
+
+    // Lists the latest from one station by URL as JSON
     @RequestMapping(value = "/latest/{stationName}", method = RequestMethod.GET)
     @ResponseBody
-    public String displayMinAndMaxByStation(@PathVariable String stationName) {
+    public ArrayList<ArrayList> displayMinAndMaxByStation(@PathVariable String stationName) {
+        ArrayList<ArrayList> displayMinAndMaxByStation = new ArrayList<>();
         Date yesterday = new Date(System.currentTimeMillis() - 1000L * 60L * 60L * 24L);
 
-        //TODO: figure out the way how to format date here also
         //Pattern and SimpleDateFormat for formating
         String pattern = "dd/MM/yyyy HH:mm";
         SimpleDateFormat df = new SimpleDateFormat(pattern);
 
-
-        // This section is for printing out the latest submitted observation for the given station
-        String observationId;
-        String observationTemp;
-        String observationTimestamp;
-        String response = "";
-
+        // Fetch all necessary data from db with value Station.valueOf(stationName)
         Iterable<Observation> observationIdByStation = observationDAO.findLatestObservationIdByStation(Station.valueOf(stationName));
         Iterable<Observation> observationTempByStation = observationDAO.findLatestTempByStation(Station.valueOf(stationName));
         Date observationTimestampByStation = observationDAO.findLatestTimestampByStation(Station.valueOf(stationName));
-
-        //Formatting out some unnecessary brackets from sql query
-        observationId = observationIdByStation.toString();
-        observationId = observationId.substring(1, observationId.length() -1);
-        observationTemp = observationTempByStation.toString();
-        observationTemp = observationTemp.substring(1, observationTemp.length() -1);
-        observationTimestamp = df.format(observationTimestampByStation).toString();
-
-        response = response + "[{\"observationId\" : \"" + observationId + "\",\"temperature\" : \"" + observationTemp + "\",\"timestamp\" : \"" + observationTimestamp + "\"},";
-
-        // And this section is for printing out the max/min temperatures for the given station for the last 24 hours
-        String observationMin;
-        String observationMax;
-
         Iterable<Observation> stationMin = observationDAO.findMinTempByStation(Station.valueOf(stationName), yesterday);
         Iterable<Observation> stationMax = observationDAO.findMaxTempByStation(Station.valueOf(stationName), yesterday);
 
-        //Formatting out some unnecessary brackets from sql query
-        observationMin = stationMin.toString();
-        observationMin = observationMin.substring(1, observationMin.length() -1);
-        observationMax = stationMax.toString();
-        observationMax = observationMax.substring(1, observationMax.length() -1);
+        // Format fetched data into a String and cut out unnecessary brackets from the beginning and end
+        // FetchData.toString().substring(1, FetchData.toString().length() -1);
+        String stationNameToCollection = Station.valueOf(stationName).toString();
+        String observationId = observationIdByStation.toString().substring(1, observationIdByStation.toString().length() -1);
+        String observationTemp = observationTempByStation.toString().substring(1, observationTempByStation.toString().length() -1);
 
-        response = response + "{\"min\" : \"" + observationMin + "\", \"max\" : \"" + observationMax + "\"}]";
-        return response;
+        String observationTimestamp;
+        try {
+            observationTimestamp = df.format(observationTimestampByStation).substring(1, df.format(observationTimestampByStation).length() -1);
+        } catch (Exception e) {observationTimestamp = "";}
+
+        String observationMin = stationMin.toString().substring(1, stationMin.toString().length() -1);
+        String observationMax = stationMax.toString().substring(1, stationMax.toString().length() -1);
+
+        // Create a list of Strings to print out and add the previous values to it
+        ArrayList<String> latestByStation = new ArrayList<String>();
+
+        latestByStation.add(stationNameToCollection);
+        latestByStation.add(observationId);
+        latestByStation.add(observationTemp);
+        latestByStation.add(observationTimestamp);
+        latestByStation.add(observationMin);
+        latestByStation.add(observationMax);
+
+        displayMinAndMaxByStation.add(latestByStation);
+
+        return displayMinAndMaxByStation;
     }
 
 }
